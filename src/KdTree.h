09dd90954c6736ac364cf3c787e65d4302c95627
Patrick Werner <boon.werner@gmail.com>
2017-05-28 13:56:51 +0200

    added SAH to kdtree building

--- src/KdTree.h
+++ src/KdTree.h
@@ -109,48 +109,111 @@ public:
 private:
     KdTreeNode root;
 
-    KdTreeNode build(std::vector<std::shared_ptr<Primitive>> primitives, const int depth, glm::vec3 min, glm::vec3 max) {
+    KdTreeNode build(const std::vector<std::shared_ptr<Primitive>> primitives, const int depth, const glm::vec3 min, const glm::vec3 max) const {
         auto node = KdTreeNode{nullptr, nullptr, nullptr, std::vector<std::shared_ptr<Primitive>>{}};
 
-        if (depth >= 5 || primitives.size() == 1) {
-            node.isLeaf = true;
-            node.data = primitives;
-            return node;
-        } else if (primitives.size() == 0) {
-            node.isLeaf = true;
-            return node;
-        }
+        node.splitAxis = Primitive::Axis(depth % 3);
 
         node.bbox = std::make_shared<BoundingBox>(BoundingBox{min, max});
         std::cout << glm::to_string(min) << " " << glm::to_string(max) << "\n";
 
-        auto axis = Primitive::Axis(depth % 3);
-        node.splitAxis = axis;
+        auto nCandidates = 50;
+        std::vector<float> candidates = std::vector<float>{};
 
-        // comparator lambda TODO getExtremes bei jedem vergleich? simpler?
-        auto compare = [&axis](std::shared_ptr<Primitive> a, std::shared_ptr<Primitive> b) {
-            auto extremesA = a->getExtremes(axis);
-            auto extremesB = b->getExtremes(axis);
-            auto positionA = extremesA.min + extremesA.max;
-            auto positionB = extremesB.min + extremesB.max;
-            return positionA < positionB;
-        };
+        switch (node.splitAxis) {
+            case Primitive::Axis::x:{
+                auto start = node.bbox->getMin().x;
+                auto stop = node.bbox->getMax().x;
+                for (auto i = 0; i < nCandidates; ++i) {
+                    candidates.push_back(start + i * (stop - start) / (nCandidates-1));
+                }
+                break;
+            }
+            case Primitive::Axis::y:{
+                auto start = node.bbox->getMin().y;
+                auto stop = node.bbox->getMax().y;
+                for (auto i = 0; i < nCandidates; ++i) {
+                    candidates.push_back(start + i * (stop - start) / (nCandidates-1));
+                }
+                break;
+            }
+            case Primitive::Axis::z:{
+                auto start = node.bbox->getMin().z;
+                auto stop = node.bbox->getMax().z;
+                for (auto i = 0; i < nCandidates; ++i) {
+                    candidates.push_back(start + i * (stop - start) / (nCandidates-1));
+                }
+                break;
+            }
+        }
 
-        // median, not real median for even number of elements
-        auto n = primitives.size() / 2;
-        std::nth_element(primitives.begin(), primitives.begin()+n, primitives.end(), compare);
+        auto bestSplitCost = std::numeric_limits<float>::max();
+        auto costTrav = 0.3f;
+        auto costIntersect = 1.0f;
+        auto w = abs(node.bbox->getMax().x - node.bbox->getMin().x);
+        auto h = abs(node.bbox->getMax().z - node.bbox->getMin().z);
+        auto d = abs(node.bbox->getMax().y - node.bbox->getMin().y);
+        auto area = 2 * (w*h + w*d + d*h);
+        auto cost = costTrav * costIntersect * area * primitives.size();
 
-        //correctly split on axis TODO: better
-        auto splitPosition = primitives[n]->getExtremes(axis).min;
-        node.splitLocation = splitPosition;
+        for (auto const& candidate : candidates) {
+            auto leftHalf = std::vector<std::shared_ptr<Primitive>>{};
+            auto rightHalf = std::vector<std::shared_ptr<Primitive>>{};
+            for (auto const& primitive : primitives) {
+                auto extremes = primitive->getExtremes(node.splitAxis);
+                if (extremes.max <= candidate) {
+                    leftHalf.push_back(primitive);
+                } else if (extremes.min >= candidate) {
+                    rightHalf.push_back(primitive);
+                } else {
+                    leftHalf.push_back(primitive);
+                    rightHalf.push_back(primitive);
+                }
+            }
+
+            auto leftW = abs(node.bbox->getMax().x - node.bbox->getMin().x);
+            auto leftD = abs(node.bbox->getMax().y - node.bbox->getMin().y);
+            auto leftH = abs(node.bbox->getMax().z - node.bbox->getMin().z);
+            auto rightW = abs(node.bbox->getMax().x - node.bbox->getMin().x);
+            auto rightD = abs(node.bbox->getMax().y - node.bbox->getMin().y);
+            auto rightH = abs(node.bbox->getMax().z - node.bbox->getMin().z);
+
+            switch (node.splitAxis) {
+                case Primitive::Axis::x:{
+                    leftW = abs(candidate - node.bbox->getMin().x);
+                    rightW = abs(node.bbox->getMax().x - candidate);
+                    break;
+                }
+                case Primitive::Axis::y:{
+                    leftD = abs(candidate - node.bbox->getMin().y);
+                    rightD = abs(node.bbox->getMax().y - candidate);
+                    break;
+                }
+                case Primitive::Axis::z:{
+                    leftH = abs(candidate - node.bbox->getMin().z);
+                    rightH = abs(node.bbox->getMax().z - candidate);
+                    break;
+                }
+            }
+
+            auto leftArea = 2 * (leftW*leftH + leftW*leftD + leftD*leftH);
+            auto rightArea = 2 * (rightW*rightH + rightW*rightD + rightD*rightH);
+            auto leftCost = costTrav * costIntersect * leftArea * leftHalf.size();
+            auto rightCost = costTrav * costIntersect * rightArea * rightHalf.size();
+            auto splitCost = leftCost + rightCost;
+            if (bestSplitCost > splitCost) {
+                bestSplitCost = splitCost;
+                node.splitLocation = candidate;
+            }
+        }
+
         auto leftHalf = std::vector<std::shared_ptr<Primitive>>{};
         auto rightHalf = std::vector<std::shared_ptr<Primitive>>{};
-
         for (auto const& primitive : primitives) {
-            auto extremes = primitive->getExtremes(axis);
-            if (extremes.max <= splitPosition) {
+            auto extremes = primitive->getExtremes(node.splitAxis);
+            if (extremes.max <= node.splitLocation) {
                 leftHalf.push_back(primitive);
-            } else if (extremes.min >= splitPosition) {
+            } else if (extremes.min >= node.splitLocation) {
                 rightHalf.push_back(primitive);
             } else {
                 leftHalf.push_back(primitive);
@@ -158,23 +221,30 @@ private:
             }
         }
 
-        auto splitVec = node.bbox->getMax();
-        switch (axis) {
-            case Primitive::Axis::x: splitVec.x = splitPosition; break;
-            case Primitive::Axis::y: splitVec.y = splitPosition; break;
-            case Primitive::Axis::z: splitVec.z = splitPosition; break;
-        }
-        if (leftHalf.size() > 0) {
-            node.leftChild = std::make_shared<KdTreeNode>(build(leftHalf, depth + 1, node.bbox->getMin(), splitVec));
-        }
-        splitVec = node.bbox->getMin();
-        switch (axis) {
-            case Primitive::Axis::x: splitVec.x = splitPosition; break;
-            case Primitive::Axis::y: splitVec.y = splitPosition; break;
-            case Primitive::Axis::z: splitVec.z = splitPosition; break;
-        }
-        if (rightHalf.size() > 0) {
-            node.rightChild = std::make_shared<KdTreeNode>(build(rightHalf, depth + 1, splitVec, node.bbox->getMax()));
+        std::cout << "lol\n";
+
+        if(bestSplitCost < cost) {
+            auto splitVec = node.bbox->getMax();
+            switch (node.splitAxis) {
+                case Primitive::Axis::x: splitVec.x = node.splitLocation; break;
+                case Primitive::Axis::y: splitVec.y = node.splitLocation; break;
+                case Primitive::Axis::z: splitVec.z = node.splitLocation; break;
+            }
+            if (leftHalf.size() > 0) {
+                node.leftChild = std::make_shared<KdTreeNode>(build(leftHalf, depth + 1, node.bbox->getMin(), splitVec));
+            }
+            splitVec = node.bbox->getMin();
+            switch (node.splitAxis) {
+                case Primitive::Axis::x: splitVec.x = node.splitLocation; break;
+                case Primitive::Axis::y: splitVec.y = node.splitLocation; break;
+                case Primitive::Axis::z: splitVec.z = node.splitLocation; break;
+            }
+            if (rightHalf.size() > 0) {
+                node.rightChild = std::make_shared<KdTreeNode>(build(rightHalf, depth + 1, splitVec, node.bbox->getMax()));
+            }
+        } else {
+            node.isLeaf = true;
+            node.data = primitives;
         }
 
         return node;
